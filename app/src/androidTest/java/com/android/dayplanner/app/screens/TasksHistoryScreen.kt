3a16f609620e4957e7a2f54f42c288fd83c76a79
Vladyslav_Shevchenk3 <Vladyslav_Shevchenko3>
2022-10-14 19:56:47 +0300

    Update screens

--- app/src/androidTest/java/com/android/dayplanner/app/screens/TasksHistoryScreen.kt
+++ app/src/androidTest/java/com/android/dayplanner/app/screens/TasksHistoryScreen.kt
@@ -1,20 +1,20 @@
 package com.android.dayplanner.app.screens
 
 import com.android.dayplanner.app.R
-import com.android.dayplanner.app.screens.ListOfTasksBasic.ListOfTasks
+import com.android.dayplanner.app.screens.ListOfTasksBasic.RecyclerOfTasks
 import io.github.kakaocup.kakao.recycler.KRecyclerView
 import io.github.kakaocup.kakao.screen.Screen
 
 class TasksHistoryScreen : Screen<TasksHistoryScreen>(), ListOfTasksBasic {
 
-    private val listOfCompetedTasks = KRecyclerView({
+    private val recyclerViewOfCompetedTasks = KRecyclerView({
         withId(R.id.recyclerView)
     }, itemTypeBuilder = {
-        itemType(::ListOfTasks)
+        itemType(::RecyclerOfTasks)
     })
 
     fun actionClickOnCompleteTaskButton(textTitle: String) {
-        actionClickOnCheckBox(listOfCompetedTasks, textTitle)
+        actionClickOnCheckBox(recyclerViewOfCompetedTasks, textTitle)
     }
 
     fun actionPressBack() {
