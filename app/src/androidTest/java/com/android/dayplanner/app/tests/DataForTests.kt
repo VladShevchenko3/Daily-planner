899d8d1a51327f2c73963f9ffbe59dc8e5a7183f
Vladyslav_Shevchenk3 <Vladyslav_Shevchenko3>
2022-10-18 22:13:54 +0300

    updated data for tests

--- app/src/androidTest/java/com/android/dayplanner/app/tests/DataForTests.kt
+++ app/src/androidTest/java/com/android/dayplanner/app/tests/DataForTests.kt
@@ -4,14 +4,9 @@ import com.android.dayplanner.app.receiveCurrentDate
 
 object DataForTests {
 
-        const val TASK_TITLE = "Create new tests"
-        const val TASK_TITLE_EXTRA1 = "Create extra1 tests"
-        const val TASK_TITLE_EXTRA2 = "Create extra2 tests"
-        const val TASK_TITLE_EXTRA3 = "Create extra3 tests"
-        const val TASK_TITLE_EXTRA4 = "Create extra4 tests"
-        const val TASK_TITLE_EXTRA5 = "Create extra5 tests"
-        const val TASK_TITLE_EXTRA6 = "Create extra6 tests"
-        const val TASK_DESCRIPTION = "Need to use Kakao framework"
-        val DATE = receiveCurrentDate()
-        val START_DATE = DATE
+    const val TASK_TITLE = "Create new tests"
+    const val TASK_TITLE_NEW = "Edit tests"
+    const val TASK_DESCRIPTION = "Need to use Kakao framework"
+    val TASK_DATE = receiveCurrentDate()
+    val START_DATE = TASK_DATE
 }
