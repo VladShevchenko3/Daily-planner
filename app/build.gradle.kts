727d2d65323cd39a224539b240944583e7f59927
Vladyslav_Shevchenk3 <Vladyslav_Shevchenko3>
2022-12-14 21:55:03 +0200

    updated gradle versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
+
+    kotlinOptions {
             jvmTarget = "11"
         }
-    }
+
 
     buildFeatures {
         dataBinding = true
